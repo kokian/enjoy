604899e82354fbbb6f93e0d4dac6104ec6f846eb
Кирилл <Kirill@MacBook-Air-Kirill.local>
2015-02-01 20:20:07 +0300

    Single View Application template

---
+++ enjoy/enjoy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  enjoy
+//
+//  Created by Кирилл on 01.02.15.
+//  Copyright (c) 2015 Кирилл. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
